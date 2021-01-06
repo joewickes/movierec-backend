555cc982cd257ea1936008d1d1bac174c9367cd2
Joe Wickes <joe.c.wickes@gmail.com>
2021-01-06 18:00:09 -0500

    Move unneeded users service method to posts service

--- src/posts/posts-service.js
+++ src/posts/posts-service.js
@@ -1,18 +1,37 @@
 const PostsService = {
-  getPosts(database, limit, offset) { // Knex instance to the database
-    return database 
-      .from('posts') // Queries posts table
-      .select('*') // Selects all posts
-      .orderBy('date_created', 'desc') // Sorts by most recent date_created
+  // getPosts(database, limit, offset) { // Knex instance to the database
+  //   return database 
+  //     .from('posts') // Queries posts table
+  //     .select('*') // Selects all posts
+      
+  //   ;
+  // },
+  getPosts(database, limit, offset) { // Knex instnce to the database
+    return database
+      .select('posts.id', 'movies.original_title as title', 'users.username')
+      .sum({votes: 'votes.value'})
+      .groupBy('posts.id', 'movies.original_title', 'users.username')
+      .from('posts')
+      .innerJoin('users', 'posts.user_id', 'users.id')
+      .innerJoin('movies', 'posts.movie_id', 'movies.id')
+      .innerJoin('votes', 'posts.id', 'votes.post_id')
+      .orderBy('posts.date_created', 'desc') // Sorts by most recent date_created
       .limit(limit) // Limits the results to the first 10
       .offset(offset) // Offsets which set of results is seen 1-10, 11-20, etc.
-    ;
   },
   addPost(database, newPostObj) {
     return database
       .insert(newPostObj) // Insert the new post object
       .into('posts') // into the posts table
       .returning('*') // returning the object
+  },
+  searchPostsByTitle(database, title, limit, offset) {
+    return database 
+      .from('posts') // Queries movies table
+      .select('*') // Selects all movies
+      .where(database.raw(`LOWER(title) LIKE LOWER('%${title}%')`)) // Searches with lowercase title against lowercase param
+      .limit(limit) // Limits the results to the first 10
+      .offset(offset) // Offsets which set of results is seen 1-10, 11-20, etc.
   }
   
 }
--- src/users/users-service.js
+++ src/users/users-service.js
@@ -5,15 +5,6 @@ const UsersService = {
       .first()
       .then(user => !!user)
   },
-  getUsers(database, param, limit, offset) { // Knex instance to the database
-    return database 
-      .from('movies') // Queries movies table
-      .select('*') // Selects all movies
-      .where(database.raw(`LOWER(original_title) LIKE LOWER('%${param}%')`)) // Searches with lowercase title against lowercase param
-      .limit(limit) // Limits the results to the first 10
-      .offset(offset) // Offsets which set of results is seen 1-10, 11-20, etc.
-    ;
-  },
   addUser(database, newUserObj) {
     return database
       .insert(newUserObj) // Insert the new user object
