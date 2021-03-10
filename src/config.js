c90a659f681dabbce0d0a2781a926548458eaf0d
Joe Wickes <joe.c.wickes@gmail.com>
2021-03-10 16:03:21 -0500

    Update testing issues with local postgres

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DATABASE_URL: `${process.env.DATABASE_URL}?ssl=true` || 'postgresql://dunder_mifflin@localhost/movierec',
+  DATABASE_URL: `${process.env.DATABASE_URL}` || 'postgresql://dunder_mifflin@localhost/movierec',
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://dunder_mifflin@localhost/movierec-test',
   JWT_SECRET: process.env.JWT_SECRET || '',
 }
