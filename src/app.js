7d9f115ea2d5fe5f80275834749fe3798b29c093
Joe Wickes <joe.c.wickes@gmail.com>
2021-03-15 00:23:04 -0400

    Remove unnecessary logs

--- src/app.js
+++ src/app.js
@@ -40,7 +40,6 @@ app.use(function errorHandler(error, req, res, next) {
   } else {
     response = { message: error.message, error }
   }
-  console.log(error, response);
   res.status(500).json(response)
 })
 
